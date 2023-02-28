68891262148c3f220751f8cc123e7fdba7259281
IVAN <107381192+ivanchristiann@users.noreply.github.com>
2023-02-28 17:34:00 +0700

    Mulai kerja halaman login

---
+++ app/src/main/java/com/jitusolution/native160420015/changePasswordActivity.kt
@@ -0,0 +1,11 @@
+package com.jitusolution.native160420015
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class changePasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+    }
+}
